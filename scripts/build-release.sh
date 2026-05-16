01765292977a3bf156b6415452bff563bf962954
Lucas Briese <jackjasonjones+github@gmail.com>
2026-05-17 00:51:43 +0200

    ci: publish semantic version images

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -8,11 +8,11 @@ RCLONE_VERSION="${3:?"Please provide Rclone version as third argument"}"
 RESTIC_VERSION="${4:?"Please provide Restic version as fourth argument"}"
 BIVAC_VERSION="${5:?"Please provide Bivac version as fifth argument"}"
 
-tags=("$BIVAC_VERSION")
+tags=("$BIVAC_VERSION" "latest")
 
 if [[ "$BIVAC_VERSION" =~ ^v?([0-9]+)\.([0-9]+)\.[0-9]+$ ]]; then
-    tags+=("${BASH_REMATCH[1]}.${BASH_REMATCH[2]}")
-    tags+=("${BASH_REMATCH[1]}")
+    tags+=("v${BASH_REMATCH[1]}.${BASH_REMATCH[2]}")
+    tags+=("v${BASH_REMATCH[1]}")
 fi
 
 cd "$(dirname "$0")/.."
